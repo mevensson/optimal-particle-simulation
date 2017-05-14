188642ad689591b64e082ab09a7dc14f7c5c4f0f
Mattias Evensson <mattias@evensson.eu>
2017-05-14 16:00:35 +0200

    Add position and velocity to Paritcle

--- src/main/java/eu/evensson/optpartsim/Particle.java
+++ src/main/java/eu/evensson/optpartsim/Particle.java
@@ -3,13 +3,69 @@ package eu.evensson.optpartsim;
 public class Particle {
 
 	private final long id;
+	private final Vector position;
+	private final Vector velocity;
 
-	public Particle(final long id) {
+	public Particle(final long id, final Vector position,
+			final Vector velocity) {
 		this.id = id;
+		this.position = position;
+		this.velocity = velocity;
 	}
 
 	public long id() {
 		return id;
+	}
+
+	public Vector position() {
+		return position;
+	}
+
+	public Vector velocity() {
+		return velocity;
+	}
+
+	// Generated methods below
+	@Override
+	public String toString() {
+		return "Particle [id=" + id + ", position=" + position + ", velocity="
+				+ velocity + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result
+				+ ((position == null) ? 0 : position.hashCode());
+		result = prime * result
+				+ ((velocity == null) ? 0 : velocity.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final Particle other = (Particle) obj;
+		if (id != other.id)
+			return false;
+		if (position == null) {
+			if (other.position != null)
+				return false;
+		} else if (!position.equals(other.position))
+			return false;
+		if (velocity == null) {
+			if (other.velocity != null)
+				return false;
+		} else if (!velocity.equals(other.velocity))
+			return false;
+		return true;
 	}
 
 }
--- src/test/java/eu/evensson/optpartsim/ParticleTest.java
+++ src/test/java/eu/evensson/optpartsim/ParticleTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.junit.platform.runner.JUnitPlatform;
@@ -17,7 +18,23 @@ public class ParticleTest {
 	@ParameterizedTest(name = "id={0}")
 	@ValueSource(longs= {1, 12345678, Long.MAX_VALUE})
 	void hasAnId(final long id) {
-		final Particle particle = new Particle(id);
+		final Particle particle = new Particle(id, null, null);
 		assertThat(particle.id(), is(id));
+	}
+
+	@DisplayName("has a Position")
+	@Test
+	void hasAPosition() {
+		final Vector position = new Vector(1.0, 2.0);
+		final Particle particle = new Particle(1, position, null);
+		assertThat(particle.position(), is(position));
+	}
+
+	@DisplayName("has a Velocity")
+	@Test
+	void hasAVelocity() {
+		final Vector velocity = new Vector(1.0, 2.0);
+		final Particle particle = new Particle(1, null, velocity);
+		assertThat(particle.velocity(), is(velocity));
 	}
 }
