1aa93977bceb53ea6f02f7c39a5200682882e9b0
Mattias Evensson <mattias@evensson.eu>
2017-05-14 16:32:05 +0200

    Refactor Vector tests

--- src/test/java/eu/evensson/optpartsim/VectorTest.java
+++ src/test/java/eu/evensson/optpartsim/VectorTest.java
@@ -3,9 +3,9 @@ package eu.evensson.optpartsim;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.api.Test;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
 
@@ -13,19 +13,25 @@ import org.junit.runner.RunWith;
 @RunWith(JUnitPlatform.class)
 public class VectorTest {
 
+	static double X = 10.0;
+	static double Y = 20.0;
+
+	Vector aVector;
+
+	@BeforeEach
+	void createVector() {
+		aVector = new Vector(X, Y);
+	}
+
 	@DisplayName("has an x value")
-	@ParameterizedTest(name = "x={0,number,0.#E0}")
-	@ValueSource(doubles = {Double.MIN_VALUE, 0.0, Double.MAX_VALUE})
-	void hasAnX(final double x) {
-		final Vector vector = new Vector(x, 0);
-		assertThat(vector.x(), is(x));
+	@Test
+	void hasAnX() {
+		assertThat(aVector.x(), is(X));
 	}
 
 	@DisplayName("has a y value")
-	@ParameterizedTest(name = "y={0,number,0.#E0}")
-	@ValueSource(doubles = {Double.MIN_VALUE, 0.0, Double.MAX_VALUE})
-	void hasAY(final double y) {
-		final Vector vector = new Vector(0, y);
-		assertThat(vector.y(), is(y));
+	@Test
+	void hasAY() {
+		assertThat(aVector.y(), is(Y));
 	}
 }
