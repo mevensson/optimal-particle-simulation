23aba5ce140a221ad672b20773369d60d1a694c7
Mattias Evensson <mattias@evensson.eu>
2017-10-02 23:13:44 +0200

    Add support for CollisionEvent in EventChecker

--- src/main/java/eu/evensson/optpartsim/simulation/EventChecker.java
+++ src/main/java/eu/evensson/optpartsim/simulation/EventChecker.java
@@ -1,6 +1,8 @@
 package eu.evensson.optpartsim.simulation;
 
+import java.util.Collection;
 import java.util.Comparator;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 import eu.evensson.optpartsim.physics.Box;
@@ -18,20 +20,47 @@ public class EventChecker {
 	public Event check(final Particle particle) {
 		return Stream.of(
 				wallBounceHorizontalEvent(particle),
-				wallBounceVerticalEvent(particle))
+				wallBounceVerticalEvent(particle),
+				collisionEvent(particle))
+			.filter(Optional::isPresent)
+			.map(Optional::get)
 			.min(Comparator.comparingDouble(event -> event.time()))
 			.get();
 	}
 
-	private Event wallBounceHorizontalEvent(final Particle particle) {
+	private Optional<Event> wallBounceHorizontalEvent(final Particle particle) {
 		final Box walls = cellStructure.getWalls();
 		final double bounceTime = particle.collisionTime(walls, Direction.HORIZONTAL);
-		return new WallBounceEvent(bounceTime, particle, Direction.HORIZONTAL);
+		return Optional.of(
+				new WallBounceEvent(bounceTime, particle, Direction.HORIZONTAL));
 	}
 
-	private Event wallBounceVerticalEvent(final Particle particle) {
+	private Optional<Event> wallBounceVerticalEvent(final Particle particle) {
 		final Box walls = cellStructure.getWalls();
 		final double bounceTime = particle.collisionTime(walls, Direction.VERTICAL);
-		return new WallBounceEvent(bounceTime, particle, Direction.VERTICAL);
+		return Optional.of(
+				new WallBounceEvent(bounceTime, particle, Direction.VERTICAL));
+	}
+
+	private Optional<Event> collisionEvent(final Particle particle) {
+		final Collection<Particle> neighbourParticles =
+				cellStructure.neighbourParticles(particle);
+		Optional<Double> earliestCollisionTime = Optional.empty();
+		Optional<Particle> collisionParticle = Optional.empty();
+		for (final Particle neighbourParticle : neighbourParticles) {
+			final Optional<Double> collisionTime =
+					particle.collisionTime(neighbourParticle);
+			if (collisionTime.isPresent()
+					&& (!earliestCollisionTime.isPresent()
+						|| collisionTime.get() < earliestCollisionTime.get())) {
+				earliestCollisionTime = collisionTime;
+				collisionParticle = Optional.of(neighbourParticle);
+			}
+		}
+		if (collisionParticle.isPresent()) {
+			return Optional.of(new CollisionEvent(earliestCollisionTime.get(),
+					particle, collisionParticle.get()));
+		}
+		return Optional.empty();
 	}
 }
--- src/test/java/eu/evensson/optpartsim/simulation/EventCheckerTest.java
+++ src/test/java/eu/evensson/optpartsim/simulation/EventCheckerTest.java
@@ -1,7 +1,11 @@
 package eu.evensson.optpartsim.simulation;
 
 import static eu.evensson.optpartsim.physics.Vector.vector;
+import static java.lang.Math.ulp;
+import static java.util.Arrays.asList;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.closeTo;
+import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -22,6 +26,7 @@ public class EventCheckerTest {
 	private static final Vector POSITION = vector(
 			WALLS.x() + WALLS.width() / 3.0,
 			WALLS.y() + WALLS.height() / 3.0);
+
 	private static final double TIME = 100.0;
 
 	private CellStructure cellStructure = mock(CellStructure.class);
@@ -100,5 +105,32 @@ public class EventCheckerTest {
 		final WallBounceEvent expectedEvent = new WallBounceEvent(
 				TIME + timeToWall, particle, Particle.Direction.VERTICAL);
 		assertThat(event, is(expectedEvent));
+	}
+
+	@DisplayName("returns collision event")
+	@Test
+	void returnsCollisionEvent() {
+		final double speed = 1.0;
+		final Vector velocity = vector(speed, 0.0);
+		final Particle particle = new Particle(ID, TIME, POSITION, velocity);
+
+		final Vector otherPosition = POSITION.add(vector(WALLS.width() / 3.0, 0.0));
+		final Vector otherVelocity = vector(-speed, 0.0);
+		final Particle otherParticle = new Particle(ID + 1, TIME, otherPosition,
+				otherVelocity);
+		when(cellStructure.neighbourParticles(particle))
+				.thenReturn(asList(otherParticle));
+
+		final Event event = anEventChecker.check(particle);
+
+		final double distanceBetweenParticles = otherPosition.x() - POSITION.x() - 2 * Particle.RADIUS;
+		final double relativeSpeed = 2.0 * speed;
+		final double timeToCollision = distanceBetweenParticles / relativeSpeed;
+		final double collisionTime = TIME + timeToCollision;
+		assertThat(event, is(instanceOf(CollisionEvent.class)));
+		final CollisionEvent collisionEvent = (CollisionEvent) event;
+		assertThat(collisionEvent.time(), is(closeTo(collisionTime, ulp(collisionTime))));
+		assertThat(collisionEvent.particle(), is(particle));
+		assertThat(collisionEvent.otherParticle(), is(otherParticle));
 	}
 }
