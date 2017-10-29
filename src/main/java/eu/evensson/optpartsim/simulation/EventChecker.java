259f859267ec595567296d845671dc7c8aec5895
Mattias Evensson <mattias@evensson.eu>
2017-10-29 18:56:33 +0100

    Convert collision event checking to use streams

--- src/main/java/eu/evensson/optpartsim/simulation/EventChecker.java
+++ src/main/java/eu/evensson/optpartsim/simulation/EventChecker.java
@@ -1,6 +1,5 @@
 package eu.evensson.optpartsim.simulation;
 
-import java.util.Collection;
 import java.util.Comparator;
 import java.util.Optional;
 import java.util.stream.Stream;
@@ -11,56 +10,44 @@ import eu.evensson.optpartsim.physics.Particle.Direction;
 
 public class EventChecker {
 
-	private CellStructure cellStructure;
+	private final CellStructure cellStructure;
 
 	public EventChecker(final CellStructure cellStructure) {
 		this.cellStructure = cellStructure;
 	}
 
 	public Event check(final Particle particle) {
-		return Stream.of(
-				wallBounceHorizontalEvent(particle),
-				wallBounceVerticalEvent(particle),
-				collisionEvent(particle))
-			.filter(Optional::isPresent)
-			.map(Optional::get)
+		return Stream.concat(
+				Stream.of(
+						wallBounceHorizontalEvent(particle),
+						wallBounceVerticalEvent(particle)),
+				collisionEvents(particle))
 			.min(Comparator.comparingDouble(event -> event.time()))
 			.get();
 	}
 
-	private Optional<Event> wallBounceHorizontalEvent(final Particle particle) {
+	private Event wallBounceHorizontalEvent(final Particle particle) {
 		final Box walls = cellStructure.getWalls();
 		final double bounceTime = particle.collisionTime(walls, Direction.HORIZONTAL);
-		return Optional.of(
-				new WallBounceEvent(bounceTime, particle, Direction.HORIZONTAL));
+		return new WallBounceEvent(bounceTime, particle, Direction.HORIZONTAL);
 	}
 
-	private Optional<Event> wallBounceVerticalEvent(final Particle particle) {
+	private Event wallBounceVerticalEvent(final Particle particle) {
 		final Box walls = cellStructure.getWalls();
 		final double bounceTime = particle.collisionTime(walls, Direction.VERTICAL);
-		return Optional.of(
-				new WallBounceEvent(bounceTime, particle, Direction.VERTICAL));
+		return new WallBounceEvent(bounceTime, particle, Direction.VERTICAL);
 	}
 
-	private Optional<Event> collisionEvent(final Particle particle) {
-		final Collection<Particle> neighbourParticles =
-				cellStructure.neighbourParticles(particle);
-		Optional<Double> earliestCollisionTime = Optional.empty();
-		Optional<Particle> collisionParticle = Optional.empty();
-		for (final Particle neighbourParticle : neighbourParticles) {
-			final Optional<Double> collisionTime =
-					particle.collisionTime(neighbourParticle);
-			if (collisionTime.isPresent()
-					&& (!earliestCollisionTime.isPresent()
-						|| collisionTime.get() < earliestCollisionTime.get())) {
-				earliestCollisionTime = collisionTime;
-				collisionParticle = Optional.of(neighbourParticle);
-			}
-		}
-		if (collisionParticle.isPresent()) {
-			return Optional.of(new CollisionEvent(earliestCollisionTime.get(),
-					particle, collisionParticle.get()));
-		}
-		return Optional.empty();
+	private Stream<Event> collisionEvents(final Particle particle) {
+		return cellStructure.neighbourParticles(particle).stream()
+				.map(neighbourParticle -> collisionEvent(particle, neighbourParticle))
+				.filter(Optional::isPresent)
+				.map(Optional::get);
+	}
+
+	private Optional<CollisionEvent> collisionEvent(final Particle particle,
+			final Particle otherParticle) {
+		return particle.collisionTime(otherParticle)
+				.map(collisionTime -> new CollisionEvent(collisionTime, particle, otherParticle));
 	}
 }
